2dd982289ab7f827c22b58c0b2c140262f45b16a
NKantapong <Kantapongsangkasuk@gmail.com>
2025-09-23 00:03:03 +0700

    stop using Framework

--- UI/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    // นำเข้า CSS ของ Tailwind ที่สร้างขึ้นมาตอนติดตั้ง
-    import '../app.css';
-
-    // นำเข้า CSS ของ FontAwesome
-    import '@fortawesome/fontawesome-free/css/all.min.css';
-</script>
-
-<slot />
